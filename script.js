f2d5690b7411cf2f29d7ecb16eaf65b93dd2cf35
Charles Day <cdayjr1997@gmail.com>
2023-06-02 00:12:35 -0400

    cleanup

--- script.js
+++ script.js
@@ -1,46 +1,57 @@
 'use strict';
 
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber;
+getNewNumber();
 let score = 20;
 let highScore = 0;
+
+function displayMessage(message) {
+  document.querySelector('.message').textContent = message;
+}
+
+function displayNumber(number) {
+  document.querySelector('.number').textContent = number;
+}
+
+function getNewNumber() {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+}
 
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
 
   if (!guess) {
-    document.querySelector('.message').textContent =
-      '🛑 No number was selected';
+    displayMessage('🛑 No number was selected');
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = 'Too High 📈';
+    displayMessage('Too High 📈');
     score--;
   } else if (guess < secretNumber && guess > 0) {
-    document.querySelector('.message').textContent = 'Too Low 📉';
+    displayMessage('Too Low 📉');
     score--;
   } else {
-    document.querySelector('.message').textContent =
-      '✅ You guessed correctly!';
+    displayMessage('✅ You guessed correctly!');
     if (score > highScore) {
       document.querySelector('.highscore').textContent = score;
     }
     document.querySelector('body').style.backgroundColor = 'green';
     document.querySelector('.number').style.width = '30rem';
-    document.querySelector('.number').textContent = secretNumber;
+    displayNumber(secretNumber);
   }
   document.querySelector('.score').textContent = score;
   if (score == 0) {
-    document.querySelector('.message').textContent = 'You lost the game';
+    displayMessage('You lost the game');
     document.querySelector('body').style.backgroundColor = 'red';
-    document.querySelector('.number').textContent = secretNumber;
+    displayNumber(secretNumber);
   }
 });
 
 document.querySelector('.again').addEventListener('click', () => {
   score = 20;
   document.querySelector('.score').textContent = score;
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  getNewNumber();
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.guess').value = '';
-  document.querySelector('.message').textContent = 'Start Guessing...';
-  document.querySelector('.number').textContent = '?';
+  displayMessage('Start Guessing...');
+  displayNumber('?');
   document.querySelector('.number').style.width = '15rem';
 });
